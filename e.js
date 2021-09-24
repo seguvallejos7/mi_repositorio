340e3860da2b9082a91a891f87ff65cc13cd160f
seguvallejos7 <seguvallejos3056@gmail.com>
2021-09-23 22:02:36 -0300

    js

---
+++ e.js
@@ -0,0 +1,7 @@
+setTimeout(()=>{
+    busco=document.getElementById(`ultimo`)
+    busco.nextElementSibling.remove()
+    busco.nextElementSibling.remove()
+    busco.nextElementSibling.remove()
+    busco.nextElementSibling.remove()
+}, 2000);
