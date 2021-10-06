c00e6b4226161b6c77b0138b81bd67a015cdaf36
seguvallejos7 <seguvallejos3056@gmail.com>
2021-10-06 19:11:22 -0300

    javascript

---
+++ app.js
@@ -0,0 +1,19 @@
+alert("Bienvenido")
+let nombre= prompt("Ingrese su nombre")
+let apellido = prompt("Ingrese su apellido")
+let edad = Number(prompt("ingrese su edad"))
+
+console.log(nombre)
+console.log(apellido)
+console.log(edad)
+
+let mensaje = prompt("¿Que mensaje quiere enviar?")
+alert(`${nombre} ${apellido}`)
+
+let num1= Number(prompt("Ingrese un numero"))
+let num2= Number(prompt("Ingrese otro numero"))
+console.log(num1+num2)
+console.log(num1-num2)
+console.log(num1*num2)
+console.log(num1/num2)
+
--- e.js
+++
@@ -1,7 +0,0 @@
-setTimeout(()=>{
-    busco=document.getElementById(`ultimo`)
-    busco.nextElementSibling.remove()
-    busco.nextElementSibling.remove()
-    busco.nextElementSibling.remove()
-    busco.nextElementSibling.remove()
-}, 1000);
